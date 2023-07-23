16ee9274676c07d9b188b3bfeb42ab67b64c54e3
Binar Nugroho <binarnugroho775@gmail.com>
2023-07-23 09:41:15 +0700

    add cors origin env variable validation

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -6,18 +6,25 @@ dotenv.config();
 
 const nodeEnv = ['production', 'development', 'test'] as const;
 
-const EnvConfig = z.object({
+const EnvConfigSchema = z.object({
   DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
   JWT_REFRESH_SECRET: z.string(),
   COOKIE_SECRET: z.string(),
   NODE_ENV: z.enum(nodeEnv),
+  CORS_ORIGINS: z
+    .string()
+    .transform((origin) => origin.split(','))
+    .default('*'),
   SERVER_PORT: z
     .string()
     .transform((port) => parseInt(port, 10))
     .default('3000'),
 });
 
-export const EnvParsedConfig = validateObjectWithZod(EnvConfig, process.env);
+export const EnvParsedConfig = validateObjectWithZod(
+  EnvConfigSchema,
+  process.env,
+);
 
 export type EnvConfigType = typeof EnvParsedConfig;
