e897427521e48aa5e4bdd9285240b48211280a23
Binar Nugroho <binarnugroho775@gmail.com>
2023-08-09 20:28:58 +0700

    fix: halaman root tidak bisa diakases

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL, Version } from '@nestjs/common';
 import { Public } from './common/decorators';
 
 @Controller()
 export class AppController {
+  @Version(VERSION_NEUTRAL)
   @Public()
   @Get()
   getHello(): string {
