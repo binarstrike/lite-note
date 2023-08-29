3f005aff59bf04188509f888bc6d2429d95447ce
Binar Nugroho <binarnugroho775@gmail.com>
2023-08-29 22:39:02 +0700

    change title and icon swagger docs page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { EnvParsedConfig as config } from './config';
+import { swaggerDocsIcon } from './swagger-ui/icon';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
@@ -8,17 +9,22 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   //* fungsi .enableVersioning() memberikan versi pada API seperti /api/v1/users/me, /api/v2/auth/signin
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: config.DEFAULT_API_VERSION });
+
   app.setGlobalPrefix('/api', { exclude: ['/'] });
+
   //* cors berguna agar API terlindungi dari request API oleh websites yang tidak dikenal
   app.enableCors({ origin: config.CORS_ORIGINS });
+
   //* module helmet berguna untuk keamanan API dengan menambahkan header khusus pada API
-  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(helmet({ contentSecurityPolicy: true }));
+
   //* pipe yang di set secara global, ValidationPipes digunakan untuk menyaring payload pada request ke API dan
   //* validasi terhadap payload agar jika terdapat kesalahan pada payload maka akan mengembalikan respon error
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  //* top middleware, cookie parser digunakan untuk mengkonversi cookie yang datang dari request menjadi object
+  //* cookie parser digunakan untuk mengkonversi cookie yang datang dari request menjadi object
   app.use(cookieParser());
 
   const swaggerConfig = new DocumentBuilder()
@@ -49,7 +55,10 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Lite Note API | Docs',
+    customfavIcon: swaggerDocsIcon,
+  });
 
   await app.listen(config.SERVER_PORT);
 }
