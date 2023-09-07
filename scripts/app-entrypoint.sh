e4805989a39b4db0fa1515c4fe398e33514abc62
Binar Nugroho <binarnugroho775@gmail.com>
2023-09-07 04:56:53 +0000

    chore: add script to detect working directory

--- scripts/app-entrypoint.sh
+++ scripts/app-entrypoint.sh
@@ -6,6 +6,14 @@ function main() {
         local command=$1; shift
         exec $command "$@"
     else
+        if [ -d $APP_WORKDIR ]; then
+            cd $APP_WORKDIR
+        elif [ -d "$PWD/dist" ]; then
+            true
+        else
+            echo >&2 "Can't find file or directory of application"
+            exit 1
+        fi
         npx prisma db push --schema ./schema.prisma
         exec node ./dist/main
     fi
